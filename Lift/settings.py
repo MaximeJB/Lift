ce743b9c69e870de3b314b78be526fb8d65486b8
MaximeJB <maxym.jb@gmail.com>
2025-11-28 15:49:29 +0100

    feat:customuser model created

--- Lift/settings.py
+++ Lift/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'liftapp',
     'rest_framework',
     'corsheaders',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -129,11 +130,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'liftapp.CustomUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100, 
+    'DEFAULT_AUTHENTICATION_CLASSES' : [
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    ]
 }
 
 CORS_ALLOWED_ORIGINS = [
--- liftapp/models.py
+++ liftapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    weight = models.DecimalField(max_digits=5, decimal_places=1, blank=True, null=True)
+    height = models.IntegerField(blank=True, null=True)
+    belt_level = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
